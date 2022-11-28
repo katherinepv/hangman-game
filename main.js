5daaa3c4635db205351673e5276478b8b83f06a4
katherine <pv.katherine7@gmail.com>
2022-11-28 22:59:14 +0000

    created function to generate random word

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const keyboard = document.querySelector(".keyboard");
+const hiddenWord = document.querySelector(".hidden-word");
 
 const alphabet = [
   "a",
@@ -29,12 +30,30 @@ const alphabet = [
   "z",
 ];
 
+const wordBank = [
+  "spaceship",
+  "cruiseship",
+  "cycling",
+  "funicular",
+  "walking",
+  "submarine",
+];
+
+// display the keyboard for users to choose letters
 const displayKeyboard = () => {
   alphabet.forEach((letter) => {
     const letterHTML = `<button>${letter}</button>`;
-    keyboard.innerHTML += letterHTML;
+    return (keyboard.innerHTML += letterHTML);
   });
 };
-displayKeyboard();
-
 console.log(displayKeyboard());
+
+// generate random word fro user to guess
+const generateRandomWord = () => {
+  let randomWord = "";
+  randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+  return randomWord;
+};
+console.log(generateRandomWord());
+
+//
